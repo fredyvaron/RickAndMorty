84ccb58a9cae62645f7e33bd1245ceafea287136
fvaron69 <fredyalberbaron@hotmail.com>
2022-06-14 12:52:00 -0500

    diseños

--- src/Component/Nav/Nav.jsx
+++ src/Component/Nav/Nav.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { search_chacter } from '../../redux/action';
-import logo from '../../Img/Logo.png';
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { search_chacter } from "../../redux/action";
+import logo from "../../Img/Logo.png";
 
 function Nav() {
-
   return (
-<nav className="navbar navbar-expand-sm navbar-light" style={{backgroundColor: "#e3f2fd"}}>
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#"><img src={logo} style={{width: "100px"}} alt='Rick And Morty'/></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="mynavbar">
-      <ul className="navbar-nav me-auto">
-        <li className="nav-item">
-          <a className="nav-link" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-      
-      </ul>
-
+    <div className="container mt-4">
+      <nav
+        className="navbar navbar-expand-sm navbar-light"
+        style={{ backgroundColor: "#e3f2fd" }}
+      >
+        <div className="container mx-auto">
+          <a className="navbar-brand" href="#">
+            <img src={logo} style={{ width: "100px" }} alt="Rick And Morty" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mynavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="mynavbar">
+            <ul className="navbar-nav  ">
+              <li className="nav-item px-2">
+                <a className="nav-link" style={{fontSize:"20px", fontWeight:"500"}} href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item px-4">
+                <a className="nav-link" style={{fontSize:"20px", fontWeight:"500"}} href="#">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
